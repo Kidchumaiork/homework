431a8e73744e17fee7ef9c77bb261fcf34a7e611
Kidchumaiork <glaosad5@gmail.com>
2022-08-02 23:40:48 -0700

    Create Exercise5_2.py

---
+++ Exercise5_2.py
@@ -0,0 +1,8 @@
+# velocity
+v = int(input("How fast is your car? (km/hr) :"))
+# time
+t = int(input("How many hours have you drive? (hr) :"))
+# displacement
+s = v * t
+print("v", "=", "s/t")
+print("s =", s, "km")
